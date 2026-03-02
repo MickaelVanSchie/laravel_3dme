2256d074e4891cc2d5d55fd8c944a21517821f7b
unknown <mickael.v.s.19@hotmail.com>
2026-03-02 22:41:56 +0100

    actually commit directive

--- app/Providers/Currency.php
+++ app/Providers/Currency.php
@@ -13,10 +13,10 @@ class Currency extends ServiceProvider
         Blade::directive('currency', function ($expression) {
             return "<?php
                 \$__args = [{$expression}];
-                \$__amount = \$__args[0] ?? 0;
+                \$amount = \$__args[0] ?? 0;
 
-                \$__formatter = new \\NumberFormatter('nl_NL', \\NumberFormatter::CURRENCY);
-                echo \$__formatter->formatCurrency((float) \$__amount, 'EUR');
+                \$formatter = new \\NumberFormatter('nl_NL', \\NumberFormatter::CURRENCY);
+                echo \$formatter->formatCurrency((float) \$amount, 'EUR');
             ?>";
         });
     }
