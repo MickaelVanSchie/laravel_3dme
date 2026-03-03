3457757b068a1fdc0c1adb11a865763cfc85ab93
Mickael van Schie <=>
2026-03-03 10:25:31 +0100

    Installed Breeze for authentication, the login/auth/dashboard page still have to get proper templaes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AddToBasketController;
 use App\Http\Controllers\BasketController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, "show"])->name('main.home');
@@ -29,6 +30,14 @@ Route::prefix('/')
         Route::get('/winkelmand', [BasketController::class, 'index'])->name('basket.index');
     });
 
-// other routes
-//require __DIR__ . '/user/account.php';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+require __DIR__ . '/auth.php';
 require __DIR__ . '/products.php';
