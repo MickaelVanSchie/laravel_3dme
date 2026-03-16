e9ffb16ffa1f385229e7cd0dfa068e8a1549d3ed
Mickael van Schie <=>
2026-03-16 17:10:58 +0100

    fixed productimagefactory

--- database/factories/ProductImageFactory.php
+++ database/factories/ProductImageFactory.php
@@ -20,7 +20,7 @@ class ProductImageFactory extends Factory
         $productIds = Product::all()->pluck('id')->toArray();
         return [
             'product_id' => fake()->randomElement($productIds),
-            'position' => fake()->randomNumber(2),
+            'position' => fake()->unique()->randomNumber(2),
             'url' => fake()->url(),
             'updated_at' => 1,
             'created_at' => 1,
